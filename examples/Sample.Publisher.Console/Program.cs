39f1a3986d23df4873452ebeebd9716e72467d8b
Jon Menzies-Smith <github@menzies-smith.co.uk>
2021-08-19 00:20:22 +0100

    Add Solution

---
+++ examples/Sample.Publisher.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Sample.Publisher.Console
+{
+    using System.IO;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args)
+                .ConfigureServices((hostBuilderContext, services) =>
+                {
+                    var startup = new Startup(hostBuilderContext.Configuration);
+                    startup.ConfigureServices(services);
+                })
+                .Build();
+
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            new HostBuilder()
+                .ConfigureAppConfiguration((hostContext, configurationBuilder) =>
+                {
+                    configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
+                    configurationBuilder.AddJsonFile("appsettings.json", optional: true);
+                    configurationBuilder.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName ?? "Production"}.json", optional: true, true);
+                    configurationBuilder.AddEnvironmentVariables(prefix: string.Empty);
+                    configurationBuilder.AddCommandLine(args);
+                });
+    }
+}
---
+++ examples/Sample.Publisher.Console/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace Sample.Publisher.Console
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static IConfiguration Configuration { get; private set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            HealthCheckConfig.ConfigureHealthChecks(services);
+        }
+    }
+}
